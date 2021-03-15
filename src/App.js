cdf22f8fd35246c33a20901d873af2c8feb3efe8
Andjelka Lukovic <andjelkal@live.com>
2021-03-15 17:20:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data'
+import Button from 'react-bootstrap/Button'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount))
+  }
+
+  const refreshLorem = () => {
+    setCount(0);
+    setText([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container text-center py-4'>
+      <h1 className='py-3'>Tired of boring lorem ipsum?</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Number of paragraphs:</label>
+        <input type='number'
+          min='1' max='10'
+          id='amount'
+          name='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <Button type='submit' variant="outline-info"
+          id="generate-btn">Generate</Button>
+      </form>
+
+      <article className='pt-3'>
+        {text.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>
+        })}
+
+        {count > 0 ? <Button onClick={refreshLorem} variant="info"
+          className="my-3">Refresh</Button> : null}
+      </article>
+    </section>
   );
 }
 
